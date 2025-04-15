d0b28b1b279e90ef83b6e0eef4d8579b372386f2
Paul Kang <kbu715@gmail.com>
2025-04-15 16:45:36 +0900

    remove: createElement

--- src/basic/util/helpers.js
+++ src/basic/util/helpers.js
@@ -1,16 +1,3 @@
-// 헬퍼 함수 모음
-const createElement = (tag, props = {}) => {
-  const element = document.createElement(tag);
-
-  for (const [key, value] of Object.entries(props)) {
-    if (value) {
-      element[key] = value;
-    }
-  }
-
-  return element;
-};
-
 const getDiscountedPrice = (amount, rate) => amount * (1 - rate);
 
 const setupDiscountTimer = (interval, delay, callback) => {
@@ -26,9 +13,4 @@ const getDiscountAlertMessage = (product, discountRate) => {
   }`;
 };
 
-export {
-  createElement,
-  getDiscountedPrice,
-  setupDiscountTimer,
-  getDiscountAlertMessage,
-};
+export { getDiscountedPrice, setupDiscountTimer, getDiscountAlertMessage };
