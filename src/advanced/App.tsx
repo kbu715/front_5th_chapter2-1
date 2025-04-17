70ee53e11766e633774f175d62316e2b6cc45176
Paul Kang <kbu715@gmail.com>
2025-04-17 18:08:16 +0900

    feat: advanced - react 개발 환경 설정

---
+++ src/advanced/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div id="container" className="bg-gray-100 p-8">
+      <div
+        id="wrapper"
+        className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8"
+      >
+        <h1 className="text-2xl font-bold mb-4">장바구니</h1>
+      </div>
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
+import path from "node:path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@advanced": path.resolve(__dirname, "./src/advanced")
+    }
+  },
   test: {
     globals: true,
     environment: "jsdom",
