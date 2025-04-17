78e2a200315d7e49ffe2c11d9d415e5c11a34b6a
Paul Kang <kbu715@gmail.com>
2025-04-17 22:53:55 +0900

    chore: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig, loadEnv } from "vite";
 import path from "node:path";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@advanced": path.resolve(__dirname, "./src/advanced")
+const mode = process.env.NODE_ENV || "development";
+const env = loadEnv(mode, process.cwd(), "");
+
+export default mergeConfig(
+  defineConfig({
+    base: env.VITE_BASE_PATH,
+    resolve: {
+      alias: {
+        "@advanced": path.resolve(__dirname, "./src/advanced")
+      }
     }
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests.js"
-  }
-});
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests.js"
+    }
+  })
+);
